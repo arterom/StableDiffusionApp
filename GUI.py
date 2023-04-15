1368806e57d9eb550ee69d429428d89b9294621e
arterom <martero@ifae.es>
2023-04-15 20:38:07 +0200

    updates

---
+++ GUI.py
@@ -0,0 +1,52 @@
+import customtkinter
+from PIL import Image, ImageTk
+import os
+
+import keras_cv
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+customtkinter.set_appearance_mode("dark") 
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("532x622")
+app.title("CustomTkinter simple_example.py")
+
+model = keras_cv.models.StableDiffusion(img_width=100, img_height=100)
+
+
+
+def generate():
+	img = model.text_to_image(prompt.get(), batch_size=1,  # How many images to generate at once
+    num_steps=50,  # Number of iterations (controls image quality)
+    seed=123)
+	Image.fromarray(img[0]).save("generated.png")
+	ph = ImageTk.PhotoImage(Image.fromarray(img[0]))
+	home_image_main.configure(image=ph)
+	#Loading previous image
+	#img = Image.open("./Manu_input.png")
+	#ph= ImageTk.PhotoImage(img)
+	#home_image_main.configure(image=ph)
+
+
+
+app = customtkinter.CTkFrame(master=app)
+app.pack(pady=20, padx=60, fill="both", expand=True)
+
+prompt = customtkinter.CTkEntry(master=app, placeholder_text="AddMe", font=("Arial",20), fg_color="grey", width=512)
+prompt.pack(pady=10, padx=10)
+
+trigger = customtkinter.CTkButton(master=app, font=("Arial",20), text_color="white", 
+	fg_color="blue", command=generate)
+trigger.pack(pady=10, padx=10)
+trigger.configure(text="Generate")
+
+
+home_image_main = customtkinter.CTkLabel(master=app,text="")
+home_image_main.pack(pady=20, padx=30)
+
+
+
+
+app.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # StableDiffusionApp
+# -------------------------
+# -------------------------
+## Setting up the environment
+Setting up conda environment with the right dependencies:
+
+$ `git clone https://github.com/arterom/StableDiffusionApp.git`
+
+$ `cd StableDiffusionApp`
+
+$ `conda env create -f conda_environment.yml`
+
+$ `conda activate app_tf_SD`
+
+# -------------------------
+# -------------------------
+
+## Quickstart Guide
+
+$ `python GUI.py`
